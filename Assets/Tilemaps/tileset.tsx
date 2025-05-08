7fc940e0827a7dec2898b0ca7c9e541ecc2684c5
Cody Liu <61233803+codster76@users.noreply.github.com>
2025-05-08 10:56:47 +1000

    Completed week 9

---
+++ Assets/Tilemaps/tileset.tsx
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilemap" tilewidth="16" tileheight="16" spacing="1" tilecount="132" columns="12">
+ <image source="kenney_tiny-dungeon/Tilemap/tilemap.png" width="203" height="186"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9.90909" y="9.18182" width="6.09091" height="6.81818"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="7.81818" width="16" height="8.18182"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="9.18182" width="5.90909" height="6.81818"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10" y="0" width="6" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="5.81818" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.0909" y="0" width="5.90909" height="9"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="9.36364"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="6.18182" height="8.81818"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="unity:IsTrigger" value="true"/>
+   <property name="unity:layer" value="Hazard"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+ </tile>
+</tileset>
