cd43c9300caaec05e90474fe383758e34bb9dc0b
Cody Liu <61233803+codster76@users.noreply.github.com>
2025-05-19 10:47:16 +1000

    Completed week 11

--- Assets/Tilemap/tilemaps/tilemap.tsx
+++ Assets/Tilemap/tilemaps/tilemap.tsx
@@ -52,4 +52,9 @@
    <object id="2" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
+ <tile id="63">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
